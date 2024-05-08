2ba3f5ff20829f0a5cfcfc73acdc46e5c1189585
ywzz3293 <50302188+ywzz3293@users.noreply.github.com>
2024-05-07 19:18:34 -0700

    build
    list model issue

--- ModernVpn/MVVM/ViewModel/ProtectionViewModel.cs
+++ ModernVpn/MVVM/ViewModel/ProtectionViewModel.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ModernVpn.MVVM.Model;
 
 namespace ModernVpn.MVVM.ViewModel
 {
     internal class ProtectionViewModel
     {
+        public ObservableCollection<ServerModel> Servers { get; set; }
         public ProtectionViewModel()
         {
-            
+            Servers = new ObservableCollection<ServerModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                Servers.Add(new ServerModel
+                {
+                    Country = "USA"
+                }) ;
+            }
         }
     }
 }
